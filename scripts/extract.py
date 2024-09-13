60ee527db1f4229ec9b85f8eee5dcb5c3a3d5f09
Bruno Alves <bruno.alves@cern.ch>
2024-09-14 01:06:48 +0200

    Updates.

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-filename = "/home/bruno/Downloads/reslimits__poi_r.npz"
+filename = "data/reslimits__poi_r.npz"
 data = np.load(filename)
 lst = data.files
 for item in lst:
     for elem in data[item]:
-        print(round(elem[1]*1000,2))
+        #print("$" + str(round(elem[1]*1000,2)) + "^{+" + str(round(elem[2]*1000,2)) + "}_{-" + str(round(elem[3]*1000,2)) + "}$")
+        print(elem)
+
