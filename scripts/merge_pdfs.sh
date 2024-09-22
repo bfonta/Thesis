0c526fbdc010f292f14c1d8141e476c96e13161c
Bruno Alves <bruno.alves@cern.ch>
2024-09-22 17:12:19 +0200

    Update MET SFs histograms.

--- scripts/merge_pdfs.sh
+++ scripts/merge_pdfs.sh
@@ -1,2 +1,3 @@
-pdftk thesis.pdf cat 1-r1 output bulk.pdf
-pdftk couverture/1ere.pdf bulk.pdf couverture/4eme.pdf cat output thesis_final.pdf
+pdftk thesis.pdf cat 2-r4 output bulk.pdf
+pdftk couverture/4eme.pdf cat 1 output back.pdf
+pdftk couverture/1ere.pdf bulk.pdf blank.pdf back.pdf cat output thesis_final.pdf
