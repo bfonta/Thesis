521ba3ccbdb79e74ad616cda90a4d859c3184691
Bruno Alves <bruno.alves@cern.ch>
2024-09-24 18:17:44 +0200

    Ready for submission.

--- scripts/merge_pdfs.sh
+++ scripts/merge_pdfs.sh
@@ -1,3 +1,3 @@
 pdftk thesis.pdf cat 2-r4 output bulk.pdf
 pdftk couverture/4eme.pdf cat 1 output back.pdf
-pdftk couverture/1ere.pdf bulk.pdf blank.pdf back.pdf cat output thesis_final.pdf
+pdftk couverture/1ere.pdf bulk.pdf back.pdf cat output thesis_final.pdf
