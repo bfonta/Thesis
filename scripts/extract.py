df5613bd1707ded89d61a7309891750a27a70113
Bruno Alves <bruno.alves@cern.ch>
2024-10-07 20:36:10 +0200

    Updates.

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-filename = "data/reslimits__poi_r.npz"
+filename = "data/reslimits__poi_r_spin2.npz"
 data = np.load(filename)
 lst = data.files
 for item in lst:
@@ -10,12 +10,12 @@ for item in lst:
 
         # convert to json format
         mes = '"' + str(elem[0]) + '": {\n'
-        mes += '    "limit_m2": ' + str(elem[5]) + ',\n'
-        mes += '    "limit_m1": ' + str(elem[3]) + ',\n'
-        mes += '    "limit_p1": ' + str(elem[2]) + ',\n'
-        mes += '    "limit_p2": ' + str(elem[4]) + ',\n'
-        mes += '    "observed": X,\n'
-        mes += '    "limit": ' + str(elem[1]) + ',\n'
+        mes += '    "limit_m2": ' + str(1000*elem[5]) + ',\n'
+        mes += '    "limit_m1": ' + str(1000*elem[3]) + ',\n'
+        mes += '    "limit_p1": ' + str(1000*elem[2]) + ',\n'
+        mes += '    "limit_p2": ' + str(1000*elem[4]) + ',\n'
+        mes += '    "observed": ' + str(1000*elem[1]) + ',\n'
+        mes += '    "limit": '    + str(1000*elem[1]) + ',\n'
         mes += '},\n'
         print(mes)
 
